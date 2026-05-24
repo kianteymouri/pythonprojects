5fb9360dd280791c031559b5cd2a6ab36db9106b
kian teymouri <96223486+kianteymouri@users.noreply.github.com>
2026-05-23 17:17:18 -0700

    Update keep_awake.py

--- keep_awake.py
+++ keep_awake.py
@@ -1,23 +1,18 @@
-#python script to keep computer awake, presses F15 key every 3 minutes
+#python script to keep computer awake, moves mouse 1 pixel left and then 1 pixel right every 3 minutes
 #to turn into .exe
 #pip install pyinstaller
 #pyinstaller --onefile keep_awake.py
 
-
 import time
-import ctypes
+import pyautogui
 
-# Virtual key code for F15
-VK_F15 = 0x7E
+INTERVAL_SECONDS = 180
 
-KEYEVENTF_KEYUP = 0x0002
-
-def press_f15():
-    ctypes.windll.user32.keybd_event(VK_F15, 0, 0, 0)
-    time.sleep(0.05)
-    ctypes.windll.user32.keybd_event(VK_F15, 0, KEYEVENTF_KEYUP, 0)
-
+print("Keep Awake is running.")
+print("Press Ctrl + C to stop.")
 
 while True:
-    press_f15()
-    time.sleep(180) #change here (in sec) for longer/shorter duration
+    pyautogui.moveRel(1, 0)
+    time.sleep(0.1)
+    pyautogui.moveRel(-1, 0)
+    time.sleep(INTERVAL_SECONDS)
